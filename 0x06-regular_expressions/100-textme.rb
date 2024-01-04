00881349526200e1f0df1313469d68d59c02872f
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-04 13:39:03 +0100

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
