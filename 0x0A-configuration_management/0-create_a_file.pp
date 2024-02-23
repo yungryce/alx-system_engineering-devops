a1877f7139afb102685ca665d8bc45967be6c400
chxgbx <chigbujoshua@yahoo.com>
2024-02-23 12:22:17 +0000

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 file { '/tmp/school':
-  ensure   => 'file',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
