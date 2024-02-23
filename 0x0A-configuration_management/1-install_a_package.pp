095a472c41ae33f0371b1401bdc66a8c55656467
chxgbx <chigbujoshua@yahoo.com>
2024-02-23 12:40:28 +0000

    Install a package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Puppet manifest to install Flask package with pip3
+# Puppet manifest to install Flask and werkzeug package with pip3
 
+# Ensure the flask specific version package is installed
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Ensure the specific version of Werkzeug is installed
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
